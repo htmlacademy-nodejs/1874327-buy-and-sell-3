c26b836e1ffedf592ea646c464ac700bb4ca75f9
Alexander Nefedyev <knighter@rambler.ru>
2021-08-24 07:39:51 +0900

    ...

--- src/express/express.js
+++ src/express/express.js
@@ -7,8 +7,10 @@ const DEFAULT_PORT = 8080;
 
 const app = express();
 
-app.set(`views`, `/templates`);
+app.set(`views`, `./src/express/templates`);
 app.set(`view engine`, `pug`);
+
+app.use(express.static('./src/express/public'));
 
 app.use(`/`, indexRouter);
 app.use(`/my`, myRouter);
