e48c02a67331803f6b4056e7b840d859fa7ef570
Alexander Nefedyev <knighter@rambler.ru>
2021-08-25 07:56:55 +0900

    Pug templated pages

--- src/express/express.js
+++ src/express/express.js
@@ -16,15 +16,8 @@ app.use(`/`, indexRouter);
 app.use(`/my`, myRouter);
 app.use(`/offers`, offersRouter);
 
-app.use((err, req, res, next) => {
-    res.status(500).res.render(`errors/500.pug`, {});
-});
+app.use((req, res, next) => res.status(404).render('errors/404.pug', {}));
 
-app.use((err, req, res, next) => {
-    res.status(404);
-
-  // respond with html page
-    res.send(404);
-});
+app.use((req, res, next) => res.status(500).render(`errors/500.pug`, {}));
 
 app.listen(DEFAULT_PORT, () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
